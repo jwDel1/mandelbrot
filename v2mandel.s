0a8a705d88fcdc30aa98b5a479c2ab88352af4a5
Dell1 <gavin71107@icloud.com>
2025-07-06 14:38:36 -0700

    increment and decrement defined as constants +basic formatting +labels finalized

--- v2mandel.s
+++ v2mandel.s
@@ -8,29 +8,33 @@
 
 _start:
 
-li s0, 1          #  
-li s1, 1          #   
-la s2, buffer     #  
-la x1, minRl      # load address of minRL into x1 
-fld fs0, 0(x1)    # load into fs0 from address stored in  x1
-la x1, minIm      # load address of minRL into x2 
-fld fs1, 0(x1)    # load into fs1 from address stored in  x2 
-la x1, zip        #
-fld fs2, 0(x1)    #
-fld fs2, 0(x1)    #
+li s0, 1            #  
+li s1, 1            #   
+
+la s2, buffer       #  
+
+la x1, minRl        # load address of minRL into x1 
+fld fs0, 0(x1)      # load into fs0 from address stored in  x1
+la x1, minIm        # load address of minRL into x2 
+fld fs1, 0(x1)      # load into fs1 from address stored in  x2 
+
+la x1, zip          #
+fld fs2, 0(x1)      #
+fld fs3, 0(x1)      #
+
+la x1, increment    #
+fld fs4, 0(x1)      #
+
+la x1, decrement    #
+fld fs5, 0(x1)      #
 
 math: 
 
-la x1, zip        #
-fld f0, 0(x1)     #
-fld f1, 0(x1)     #
-fld f2, 0(x1)     # 
-fld f3, 0(x1)     # Initialize temp registers to 0
-
-# 
-rowIterate:
-
-# Are saved registers necesary? Yes for the row number not for the column number 
+la x1, zip          #
+fld f0, 0(x1)       #
+fld f1, 0(x1)       #
+fld f2, 0(x1)       # 
+fld f3, 0(x1)       # Initialize temp registers to 0
 
 storeStar:
 
@@ -40,17 +44,19 @@ storeBlank:
 
 
 
+nextRl:
+
+
+
+printingRow:
+
+
+
 nextRow:
 
-# Branch to store Star and Store blank
-# 
 
-printingLoop:
 
-# Compare number of rows to 41 and if equal branch to a system exit
-# Print each row one at a time 
-#  
-exitProgram:
+exit:
 
  
 
@@ -67,6 +73,8 @@ exitProgram:
     minRl: .double -2.0
 
     minIm: .double -1.0 
+
+    decrement: .double -0.05
 
     zip: .double 0.0              # Big shoutout to Paul G. Hewitt
 
