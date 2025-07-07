61511cfe822e8751b27fc3340a61471536048627
Dell1 <gavin71107@icloud.com>
2025-07-06 17:40:14 -0700

    added computation for binomial expansion of z and adding c to that new z

--- v2mandel.s
+++ v2mandel.s
@@ -15,10 +15,10 @@ la s2, buffer       #
 
 la x1, minRl        # load address of minRL into x1 
 fld fs0, 0(x1)      # load into fs0 from address stored in  x1
-la x1, minIm        # load address of minRL into x2 
+la x1, maxIm        # load address of minRL into x2 
 fld fs1, 0(x1)      # load into fs1 from address stored in  x2 
 
-la x1, zip          #
+la x1, zip          # for storing 
 fld fs2, 0(x1)      #
 fld fs3, 0(x1)      #
 
@@ -31,10 +31,21 @@ fld fs5, 0(x1)      #
 math: 
 
 la x1, zip          #
-fld f0, 0(x1)       #
-fld f1, 0(x1)       #
-fld f2, 0(x1)       # 
-fld f3, 0(x1)       # Initialize temp registers to 0
+fld f2, 0(x1)       # Initialize temp registers to 0 
+ 
+fmul.d f2, f0, f1   # 
+la x1, two          # 
+fld f31, 0(x1)      #
+fmul.d f2, f2, f31  # store the middle term in a+bi binomial expansion (2(a)(bi))
+
+fmul.d f1, f1, f1   # square the bi term in binomial expansion
+fadd.d f1, f1, f3   # add the squared term to our middle term -> register f1 for new bi term
+
+fmul.d f0, f0, f0   # square our real term in binomial expansion
+
+fadd.d f0, f0, fs0  # add the real part of c to our real part of z 
+fadd.d f1, f1, fs1  # add the Im part of c to Im part of z
+
 
 storeStar:
 
@@ -110,9 +121,18 @@ exit:
 
 # GNRL PURPOSE INT
 
-# x1:
+# x1: temp var mostly used for loading addresses to fld instruction 
 
 # FLOAT PT
+
+# f0: Rl value for the iteration
+# f1: Im value for the iteration 
+# f2: stores the middle term of binomial expansion (temporarily)
+# f3
+# f4
+# f5
+
+# f31: general purpose temp var
 
 # fs0: current rl
 # fs1: current Im 
