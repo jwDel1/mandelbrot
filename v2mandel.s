75d28a88fbc2c0253fa4d8d742973668aeb4f69b
jwDel1 <jacksonwaldmang@gmail.com>
2025-07-11 07:40:52 -0700

    added indentation for readability

--- v2mandel.s
+++ v2mandel.s
@@ -8,129 +8,129 @@
 
 _start:
 
-mv s0, x0                 # cntr for rL axis 
-mv s1, x0                 # cntr for Im axis 
-mv s2, x0                 # cntr for z iterations in math segment 
-mv x1, x0                 # initialize x1 
+  mv s0, x0                 # cntr for rL axis 
+  mv s1, x0                 # cntr for Im axis 
+  mv s2, x0                 # cntr for z iterations in math segment 
+  mv x1, x0                 # initialize x1 
 
-la s3, buffer             #  
+  la s3, buffer             #  
 
-la x1, minRl              # load address of minRL into x1 
-fld fs0, 0(x1)            # load into fs0 from address stored in  x1
-la x1, maxIm              # load address of minRL into x2 
-fld fs1, 0(x1)            # load into fs1 from address stored in  x2 
+  la x1, minRl              # load address of minRL into x1 
+  fld fs0, 0(x1)            # load into fs0 from address stored in  x1
+  la x1, maxIm              # load address of minRL into x2 
+  fld fs1, 0(x1)            # load into fs1 from address stored in  x2 
 
-la x1, zip                # for storing 
-fld fs2, 0(x1)            # magnitude of z iteration
-fld fs3, 0(x1)            #
+  la x1, zip                # for storing 
+  fld fs2, 0(x1)            # magnitude of z iteration
+  fld fs3, 0(x1)            #
 
-la x1, increment          #
-fld fs4, 0(x1)            #
+  la x1, increment          #
+  fld fs4, 0(x1)            #
 
-la x1, decrement          #
-fld fs5, 0(x1)            #
+  la x1, decrement          #
+  fld fs5, 0(x1)            #
 
 math: 
 
-la x1, zip                #
-fld f2, 0(x1)             # Initialize temp registers to 0 
-fld f3, 0(x1)             #
+  la x1, zip                #
+  fld f2, 0(x1)             # Initialize temp registers to 0 
+  fld f3, 0(x1)             #
 
-fmul.d f2, f0, f1         # 
-la x1, two                # 
-fld f31, 0(x1)            #
-fmul.d f2, f2, f31        # store the middle term in a+bi binomial expansion (2(a)(bi))
+  fmul.d f2, f0, f1         # 
+  la x1, two                # 
+  fld f31, 0(x1)            #
+  fmul.d f2, f2, f31        # store the middle term in a+bi binomial expansion (2(a)(bi))
 
-fmul.d f1, f1, f1         # square the bi term in binomial expansion
-fadd.d f1, f1, f3         # add the squared term to our middle term -> register f1 for new bi term
+  fmul.d f1, f1, f1         # square the bi term in binomial expansion
+  fadd.d f1, f1, f3         # add the squared term to our middle term -> register f1 for new bi term
 
-fmul.d f0, f0, f0         # square our real term in binomial expansion
+  fmul.d f0, f0, f0         # square our real term in binomial expansion
 
-fadd.d f0, f0, fs0        # add the real part of c to our real part of z 
-fadd.d f1, f1, fs1        # add the Im part of c to Im part of z
+  fadd.d f0, f0, fs0        # add the real part of c to our real part of z 
+  fadd.d f1, f1, fs1        # add the Im part of c to Im part of z
 
-fmul.d f2, f0, f0         # 
-fmul.d f3, f1, f1         #
-fadd.d f2, f2, f3         #
-fsqrt.d fs2, f2           #
+  fmul.d f2, f0, f0         # 
+  fmul.d f3, f1, f1         #
+  fadd.d f2, f2, f3         #
+  fsqrt.d fs2, f2           #
 
-la x1, two                #
-fld f31, 0(x1)            # load 2.0 into register f31
-fge.d x1, fs2, f31        # compare our magnitude to f31
+  la x1, two                #
+  fld f31, 0(x1)            # load 2.0 into register f31
+  fge.d x1, fs2, f31        # compare our magnitude to f31
 
-li x2, 1                  # 
+  li x2, 1                  # 
 
-beq x1, x2, storeBlank    # if the bool stored in x1 from the compare isn't one we know that our magnitude exceeds two and therfor the set diverges (excluded) for this c value 
+  beq x1, x2, storeBlank    # if the bool stored in x1 from the compare isn't one we know that our 
+                            # magnitude exceeds two and therfor the set diverges (excluded) for this c value   
 
-li x1, 19                 # load 19 into x1 to compare to z counter
+  li x1, 19                 # load 19 into x1 to compare to z counter
 
-beq s2, x1, placeStar     # branch to inclusion in set if max iterations are reached 
+  beq s2, x1, placeStar     # branch to inclusion in set if max iterations are reached 
 
-addi s2, s2, 1            # Increment the count of z iterations
+  addi s2, s2, 1            # Increment the count of z iterations
 
-j math                    # If no branch repeat
+  j math                    # If no branch repeat
 
 storeStar:
 
-li x1, s2               # load our buffer address into x1 
-add x1, s2, s0          # add the Rl value counter to our buffer address serving as an offset
-lb star, 0(x1)          # store a star in register address s2 + s0 or s2 + 1n 
+  li x1, s2               # load our buffer address into x1 
+  add x1, s2, s0          # add the Rl value counter to our buffer address serving as an offset
+  lb star, 0(x1)          # store a star in register address s2 + s0 or s2 + 1n 
 
-mv x1, x0               #  
+  mv x1, x0               #  
 
-li x1, WIDTH            #
-beq s0, x1, printRow    # Check if s0 is at 81
+  li x1, WIDTH            #
+  beq s0, x1, printRow    # Check if s0 is at 81
 
-addi s0, s0, 1          # Increment Rl value counter
+  addi s0, s0, 1          # Increment Rl value counter
 
-j nextRl                # jump to our increment 
+  j nextRl                # jump to our increment 
 
 storeBlank:
 
-li x1, s2               # load our buffer address into x1 
-add x1, s2, s0          # add the Rl value counter to our buffer address serving as an offset
-lb empty, 0(x1)          # store a star in register address s2 + s0 or s2 + 1n 
+  li x1, s2               # load our buffer address into x1 
+  add x1, s2, s0          # add the Rl value counter to our buffer address serving as an offset
+  lb empty, 0(x1)         # store a star in register address s2 + s0 or s2 + 1n 
 
-mv x1, x0               #  
+  mv x1, x0               #  
 
-li x1, WIDTH            #
+  li x1, WIDTH            #
 
-la x1, s2               #
-mv x2, x0               #
+  la x1, s2               #
+  mv x2, x0               #
 
-li x1, WIDTH            #
-beq s0, x1, printRow    # Check if s0 is at 81
+  li x1, WIDTH            #
+  beq s0, x1, printRow    # Check if s0 is at 81
 
-addi s0, s0, 1          # Increment Rl value counter
+  addi s0, s0, 1          # Increment Rl value counter
 
-j nextRl                # jump to our increment 
+  j nextRl                # jump to our increment 
 
 nextRl:
 
-la increment, x1
-fld f31, 0(x1)
-fadd.d fs0, fs0, f31
+  la increment, x1        #
+  fld f31, 0(x1)          #
+  fadd.d fs0, fs0, f31    #
 
-j math
+  j math                  #
 
 printRow:
 
-la x1, s2               # load buffer address into x1 
+  la x1, s2               # load buffer address into x1 
 
-li a7, 4                # syscall for print
-mv a0, x1               # mem address for buffer into a0 
+  li a7, 4                # syscall for print
+  mv a0, x1               # mem address for buffer into a0 
 
-ecall                   #  
+  ecall                   #  
 
+  addi x2, x2, 1          #
+  add x1, x2              #
 
-addi x2, x2, 1          #
-add x1, x2              #
+  li x1, WIDTH            #
 
-li x1, WIDTH            #
+  beq x2, x1, nextRow     #
 
-beq x2, x1, nextRow     #
-
-j printRow              #
+  j printRow              #
 
 nextRow:
 
